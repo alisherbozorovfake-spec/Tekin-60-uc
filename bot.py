bfce7c2a570bb56915df0f35ceebe35d83d1c980
alisherbozorovfake-spec <alisherbozorovfake@gmail.com>
2026-02-02 11:09:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def start(msg: types.Message):
         await msg.answer("❗ Botdan foydalanish uchun kanalga obuna bo‘ling:", reply_markup=btn)
         return
 
-    await msg.answer("🎰 Random botga xush kelibsiz!\n\n🎁 Kuniga 2 marta aylantira olasiz.")
+    await msg.answer("🎰 Random botga xush kelibsiz!\n\n🎁 Kuniga 3 marta aylantira olasiz.")
 
 @dp.message_handler(commands=["spin"])
 async def spin(msg: types.Message):
